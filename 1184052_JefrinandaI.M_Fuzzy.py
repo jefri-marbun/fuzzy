42a2f5d046209da250b44b17d3789afdb7cb0092
jefri-marbun <80641117+jefri-marbun@users.noreply.github.com>
2021-04-23 11:32:12 +0700

    Add files via upload

---
+++ 1184052_JefrinandaI.M_Fuzzy.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import skfuzzy as fuzz
+
+colors = ['b', 'orange', 'g', 'r', 'c', 'm', 'y', 'k', 'Brown', 'ForestGreen']
+
+# Define three cluster centers
+centers = [[4, 2],
+           [1, 7],
+           [5, 6]]
+
+# Define three cluster sigmas in x and y, respectively
+sigmas = [[0.8, 0.3],
+          [0.3, 0.5],
+          [1.1, 0.7]]
+
+np.random.seed(42)  # Set seed for reproducibility
+xpts = np.zeros(1)
+ypts = np.zeros(1)
+labels = np.zeros(1)
+for i, ((xmu, ymu), (xsigma, ysigma)) in enumerate(zip(centers, sigmas)):
+    xpts = np.hstack((xpts, np.random.standard_normal(200) * xsigma + xmu))
+    ypts = np.hstack((ypts, np.random.standard_normal(200) * ysigma + ymu))
+    labels = np.hstack((labels, np.ones(200) * i))
+
+fig0, ax0 = plt.subplots()
+for label in range(3):
+    ax0.plot(xpts[labels == label], ypts[labels == label], '.',
+             color=colors[label])
+ax0.set_title('Test data: 200 points x3 clusters.')
+
+fig1, axes1 = plt.subplots(3, 3, figsize=(8, 8))
+alldata = np.vstack((xpts, ypts))
+fpcs = []
+
+for ncenters, ax in enumerate(axes1.reshape(-1), 2):
+    cntr, u, u0, d, jm, p, fpc = fuzz.cluster.cmeans(
+        alldata, ncenters, 2, error=0.005, maxiter=1000, init=None)
+
+    # Store fpc values for later
+    fpcs.append(fpc)
+
+    # Plot assigned clusters, for each data point in training set
+    cluster_membership = np.argmax(u, axis=0)
+    for j in range(ncenters):
+        ax.plot(xpts[cluster_membership == j],
+                ypts[cluster_membership == j], '.', color=colors[j])
+
+    # Mark the center of each fuzzy cluster
+    for pt in cntr:
+        ax.plot(pt[0], pt[1], 'rs')
+
+    ax.set_title('Centers = {0}; FPC = {1:.2f}'.format(ncenters, fpc))
+    ax.axis('off')
+
+fig1.tight_layout()
+
+cntr, u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(
+    alldata, 3, 2, error=0.005, maxiter=1000)
+
+# Show 3-cluster model
+fig2, ax2 = plt.subplots()
+ax2.set_title('Trained model')
+for j in range(3):
+    ax2.plot(alldata[0, u_orig.argmax(axis=0) == j],
+             alldata[1, u_orig.argmax(axis=0) == j], 'o',
+             label='series ' + str(j))
+ax2.legend()
+
+newdata = np.random.uniform(0, 1, (1100, 2)) * 10
+
+u, u0, d, jm, p, fpc = fuzz.cluster.cmeans_predict(
+    newdata.T, cntr, 2, error=0.005, maxiter=1000)
+
+cluster_membership = np.argmax(u, axis=0)
+
+fig3, ax3 = plt.subplots()
+ax3.set_title('Random points classified according to known centers')
+for j in range(3):
+    ax3.plot(newdata[cluster_membership == j, 0],
+             newdata[cluster_membership == j, 1], 'o',
+             label='series ' + str(j))
+ax3.legend()
+
+plt.show()
